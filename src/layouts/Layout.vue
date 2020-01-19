4f2a154584c9227a28cc5fd70364234db2f26938
NICHOLAS <nicholaszuccarelli@gmail.com>
2020-01-19 16:02:50 +1100

    Initial Quasar commit

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer side="left" v-model="dialogOpen">
+      <div>lol</div>
+    </q-drawer>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis delectus, doloremque minima perferendis quisquam veritatis. A consequatur corporis dolore facilis, ipsum iure laboriosam minima optio quas rem sapiente veritatis voluptatem.
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialogOpen: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
